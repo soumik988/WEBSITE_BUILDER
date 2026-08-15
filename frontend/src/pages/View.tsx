dd6b4350c70386e7aa71969dcf51629fb60aea17
iamsoumik <153822678+soumik988@users.noreply.github.com>
2026-08-15 19:50:50 +0530

    Fix production API URL and frontend build

--- frontend/src/pages/View.tsx
+++ frontend/src/pages/View.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyProjects } from '../assets/assets';
 import { Loader2Icon } from 'lucide-react';
