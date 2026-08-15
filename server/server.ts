83e58f1715ee744abf39c64f04228d4bc0b8d1f0
iamsoumik <153822678+soumik988@users.noreply.github.com>
2026-08-15 17:29:16 +0530

    "Configure backend for Vercel"

--- server/server.ts
+++ server/server.ts
@@ -6,12 +6,13 @@ import { auth } from "./liib/auth.js";
 import userRouter from "./routes/userRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
 
-const app = express();
+export const app = express();
 
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: process.env.TRUSTED_ORIGINS?.split(",") || ["http://localhost:5173"],
+  origin:
+    process.env.TRUSTED_ORIGINS?.split(",") || ["http://localhost:5173"],
   credentials: true,
 };
 
@@ -21,16 +22,18 @@ app.use(express.json());
 
 // Better Auth
 app.all("/api/auth/{*any}", toNodeHandler(auth));
-app.use(express.json({limit:'50mb'}))
+app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/project',projectRouter);
+app.use("/api/user", userRouter);
+app.use("/api/project", projectRouter);
 
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// Local development only
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+}
